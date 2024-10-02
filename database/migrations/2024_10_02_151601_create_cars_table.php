d462c3815605ee3df899d7a5973acf72dacfbb25
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-10-02 18:32:44 +0200

    [FEAT] created 'cars' migration and seeder

---
+++ database/migrations/2024_10_02_151601_create_cars_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 30);
+            $table->string('model', 30);
+            $table->year('year');
+            $table->string('color', 15);
+            $table->decimal('price', 10,2);
+            $table->string('engine', 30);
+            $table->smallInteger('horsepower');
+            $table->tinyInteger('doors');
+            $table->mediumInteger('km');
+            $table->boolean('used')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
