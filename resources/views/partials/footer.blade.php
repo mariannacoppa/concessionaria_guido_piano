5537935998c3d491c72dca28dfea16afda51d6d8
B0G0N1 <michelebogoni2001@gmail.com>
2024-10-10 13:20:23 +0200

    [FEAT] Add style header/footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container-fluid">
+        <p class="mb-0">&copy; 2024 GuidoPiano. Tutti i diritti riservati.</p>
+    </div>
+</footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,33 +1,29 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<header class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand" href="{{ route('welcome') }}">
+            <span class="logo-text"><span class="logo-highlight">Guido</span>Piano</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{ route('welcome') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('cars.index') }}">Cars</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                    <a class="nav-link" href="{{ route('cars.index') }}">Auto</a>
                 </li>
             </ul>
             <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <input class="form-control me-1" type="search" placeholder="Cerca" aria-label="Cerca">
+                <button class="btn btn-outline-light" type="submit">Cerca</button>
             </form>
+            <div class="d-flex align-items-center ms-2">
+                <a href="#" class="btn btn-outline-light">Accedi</a>
+                <img src="https://flagcdn.com/16x12/it.png" alt="Bandiera Italiana" class="ms-2">
+            </div>
         </div>
     </div>
-</nav>
+</header>
